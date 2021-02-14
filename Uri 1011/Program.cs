d7cbc8ac036844747b2e3d64cb5d8933090dd763
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:45:35 -0300

    Add files via upload

---
+++ Uri 1011/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace Uri_1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, r,volume;
+
+            pi = 3.14159;
+
+
+            Console.WriteLine();
+            Console.Write("Digite o Raio : ");
+            r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            r = Math.Pow(r,3);
+
+            volume = (double)((4 / 3.0) * pi * r);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("O Volume é  : "+volume.ToString("F3",CultureInfo.InvariantCulture));
+        }
+    }
+}
